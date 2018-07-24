e43de02c8a202ad569d0068cb5bdba10de1dc407
Overclocker326 <33238166+Overclocker326@users.noreply.github.com>
2018-07-24 08:35:45 -0400

    I think I broke it

--- mainManual.py
+++ mainManual.py
@@ -1,7 +1,8 @@
-from os import listdir
 import filecmp
 import os
 from shutil import copyfile
+import time
+import glob
 
 needToChange = ["deleteThis"]
 del needToChange[0]
@@ -17,29 +18,33 @@ def getFinLoc():
 
 def listFolder(folderA):
     # onlyfiles = [f for f in listdir(folderA) if isfile(join(FolderA, f))]
-    return os.listdir(folderA)
+    for x in glob.iglob(folderA + "/*", recursive=True):
+
+        #return filename
 
 def checkSame(fileA, fileB):
     return filecmp.cmp (fileA, fileB)
 
-cont = 'y'
-while cont == 'y':
-    folderA = getInitLoc()
-    folderB = getFinLoc()
 
-    for x in listFolder(folderA):
-        print(x)
+def primary(folderA, folderB):
 
-    for x in listFolder(folderA):
+    for x in os.walk(folderA):
         try:
             if checkSame(folderA + "/"+ x, folderB + "/"+ x) == True:
                 needToChange.append(x)
         except FileNotFoundError:
-            copyfile(folderA + "/"+ x, folderB)
-        else:
-            break
-
-    cont = input("Would you like to add another folder? If yes answer Y if no answer N")
+            print("Could not find file " + x + "in " + folderB + " putting it in " + folderB)
+            copyfile(folderA + "/" + x, folderB + "/" + x)
 
     for x in needToChange:
-        copyfile(folderA + "/"+ x, folderB)
+        copyfile(folderA + "/"+ x, folderB + "/"+ x)
+
+folderA = getInitLoc()
+folderB = getFinLoc()
+
+while True:
+    primary(folderA, folderB)
+    time.sleep(20)
+
+
+
